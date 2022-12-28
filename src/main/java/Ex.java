6ff84f55284c2047290cd7d7c1facdc865962147
1111jsh <zlip437@gmail.com>
2022-12-28 14:43:12 +0900

    example

---
+++ src/main/java/Ex.java
@@ -0,0 +1,23 @@
+package main.java;
+
+public class Ex {
+}
+
+ class Person {
+
+    private String name;
+
+    private  String mbti;
+
+    private String  job;
+
+    private  String age;
+
+
+     public Person(String name, String mbti, String job, String age) { // alt  +  insert
+         this.name = name;
+         this.mbti = mbti;
+         this.job = job;
+         this.age = age;
+     }
+ }
